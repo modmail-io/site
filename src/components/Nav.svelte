396a3fb1a3754b0ca9c150ac1709602d0c7317da
vcokltfre <vcokltfre@gmail.com>
2022-05-05 21:18:30 +0100

    feat: initial commit

---
+++ src/components/Nav.svelte
@@ -0,0 +1,58 @@
+<div class="nav">
+  <a href="/">
+    <img src="logo.svg" alt="Modmail logo" />
+    <h2>Modmail</h2>
+  </a>
+  <div class="grow" />
+  <div class="nav-links">
+    <a class="nav-link" href="/docs">Docs</a>
+    <a class="nav-link" href="https://github.com/modmail-io">GitHub</a>
+  </div>
+</div>
+
+<style lang="scss">
+  $height: 2.5rem;
+
+  .nav {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    height: $height;
+    background-color: lighten($col-bg, 4%);
+
+    padding: 0 0.5rem;
+
+    a,
+    .nav-links {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      text-decoration: none;
+      color: $col-text;
+
+      img {
+        height: calc($height - ($height / 7));
+        margin-right: 0.5rem;
+      }
+    }
+
+    .nav-link {
+      height: $height;
+      padding: 0 1rem;
+
+      &:hover {
+        background-color: lighten($col-bg, 8%);
+      }
+
+      &:active {
+        background-color: lighten($col-bg, 12%);
+      }
+    }
+  }
+
+  @media (min-width: 768px) {
+    .nav {
+      padding: 0 20%;
+    }
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Nav from "../components/Nav.svelte";
+</script>
+
+<svelte:head>
+  <title>Modmail - Home</title>
+</svelte:head>
+
+<Nav />
+
+<div class="soon">
+  <h1>Coming Soon</h1>
+</div>
